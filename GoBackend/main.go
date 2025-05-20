85c1c9f9b4aa1451648ddbae82aab41fbf2b2a93
Dikin19 <muhamadsodikin191919@gmail.com>
2025-05-21 04:05:34 +0700

    jwt&hashPassword

---
+++ GoBackend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"MuhamadSodikin/backend-api/config"
+	"MuhamadSodikin/backend-api/database"
+	"MuhamadSodikin/backend-api/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//load config .env
+	config.LoadEnv()
+
+	//inisialisasi database
+	database.InitDB()
+
+	//inisialiasai Gin
+	r := routes.SetupRouter()
+
+	// //membuat route dengan method GET
+	// router.GET("/", func(c *gin.Context) {
+
+	// 	//return response JSON
+	// 	c.JSON(200, gin.H{
+	// 		"message": "Hello World!",
+	// 	})
+	// })
+
+	//mulai server
+	router.Run(":" + config.GetEnv("APP_PORT", "3000"))
+}
